67967638b8d7b805e4183e5bb590142133467e54
LikynAI <Likyn2001@gmail.com>
2018-10-30 19:35:20 +0300

    Создал отделбный класс для удобства работы с конвертером

---
+++ Converter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+	class Converter
+	{
+		static int a = 0;
+		public static int to01(int v)
+		{
+			if (v != 0)
+			{
+				a += to01(v / 2) * 10 + v % 2;
+			}
+			return a;
+		}
+	}
+}
