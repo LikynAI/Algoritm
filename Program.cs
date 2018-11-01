62fb66baa27ab4a1b24045015d4cf112dfca0be1
LikynAI <Likyn2001@gmail.com>
2018-11-01 19:11:58 +0300

    Еще немного доработал

--- Program.cs
+++ Program.cs
@@ -20,6 +20,12 @@ namespace ConsoleApp3
 			Console.ReadLine();
 		}
 
+		/// <summary>
+		/// Осуществляет поиск элемента в массиве и возвпащает его индекс
+		/// </summary>
+		/// <param name="a"></param>
+		/// <param name="b"></param>
+		/// <returns></returns>
 		private static int BinSearch(int[] a, int b)
 		{
 			int left = 0;
@@ -35,8 +41,11 @@ namespace ConsoleApp3
 			return -1;
 		}
 
-
-
+		/// <summary>
+		/// Обычная сортировка пузырьком
+		/// </summary>
+		/// <param name="a"></param>
+		/// <returns></returns>
 		private static int[] Puzirok(int []a)
 		{
 			for (int i = 0; i < a.Length; i++)
@@ -54,6 +63,35 @@ namespace ConsoleApp3
 			return a;
 		}
 
+		/// <summary>
+		/// Сортировка пузырьком возвращающая к-во этераций
+		/// </summary>
+		/// <param name="a"></param>
+		/// <returns></returns>
+		private static int PuzirokCount(int[] a)
+		{
+			int Count = 0;
+			for (int i = 0; i < a.Length; i++)
+			{
+				for (int j = 0; j < a.Length - 1; j++)
+				{
+					Count++;
+					if (a[j] > a[j + 1])
+					{
+						int temp = a[j];
+						a[j] = a[j + 1];
+						a[j + 1] = temp;
+					}
+				}
+			}
+			return Count;
+		}
+
+		/// <summary>
+		/// Улучшенная сортировка пузырьком
+		/// </summary>
+		/// <param name="a"></param>
+		/// <returns></returns>
 		private static int[] PuzirokV2(int[] a)
 		{
 			
@@ -79,6 +117,11 @@ namespace ConsoleApp3
 			return a;
 		}
 
+		/// <summary>
+		/// Улучшенная шейкерная сортировка
+		/// </summary>
+		/// <param name="a"></param>
+		/// <returns></returns>
 		private static int[] Shaker(int[] a)
 		{
 			int i = 0;
@@ -114,6 +157,10 @@ namespace ConsoleApp3
 			return a;
 		}
 
+		/// <summary>
+		/// Функция вывода массива в консоль
+		/// </summary>
+		/// <param name="a"></param>
 		public static void Show(int[] a)
 		{
 			foreach (int item in a)
@@ -122,6 +169,11 @@ namespace ConsoleApp3
 			}
 		}
 
+		/// <summary>
+		/// Создает массив и заполняет его рандомными числами
+		/// </summary>
+		/// <param name="v"></param> размер
+		/// <returns></returns>
 		public static int[] CreateMassive(int v)
 		{
 			int[] mass = new int[v];
