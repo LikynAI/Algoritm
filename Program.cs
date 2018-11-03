4a4ba24ad949db7e63397276e238890919c26c35
LikynAI <Likyn2001@gmail.com>
2018-11-03 22:11:16 +0300

    Решил задачу о ходе коня

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[,] doska = new int[8,8];
+
+			for (int i = 0; i < 8; i++)
+			{
+				for (int j = 0; j < 8; j++)
+				{
+					doska[i, j] = 0;
+				}
+			}
+
+			int[] vozvrati = new int[65];
+			foreach (int i in vozvrati)
+			{
+				vozvrati[i] = 0;
+			}
+
+			
+			kon kon1 = new kon(0,0);
+
+			Stack<kon> hodi = new Stack<kon>(); 
+			hodi.Push(new kon(kon1.x,kon1.y));
+
+			int step = 0;
+			while (step != 63)
+			{
+				doska[kon1.x, kon1.y] = 1;
+				if (kon1.move(doska, vozvrati[hodi.Count]))
+				{
+					kon kont = new kon(kon1.x, kon1.y);
+					hodi.Push(kont);
+					step++;
+				}
+				else 
+				{
+					doska[kon1.x, kon1.y] = 0;
+
+					step--;
+
+					vozvrati[hodi.Count] = 0;
+
+					hodi.Pop();
+					kon kont = new kon(hodi.Peek());
+					kon1 = kont;
+
+					vozvrati[hodi.Count]++;
+				}
+			}
+
+			int o = 0;
+			foreach (kon k in hodi)
+			{
+				Console.SetCursorPosition(k.x*3, k.y*2);
+				Console.Write(++o);
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
---
+++ kon.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+	public class kon
+	{
+		public int x, y;
+
+		public kon(int x, int y)
+		{
+			this.x = x;
+			this.y = y;
+		}
+
+		public kon(kon kon1)
+		{
+			x = kon1.x;
+			y = kon1.y;
+		}
+
+		/// <summary>
+		/// передвигает коня на клетку
+		/// </summary>
+		/// <param name="mas"></param>
+		/// <param name="a"></param> указывает сколько ближайших клеток должен пропустить конь
+		/// <returns></returns>
+		public bool move( int[,] mas, int a)
+		{
+			int tempo = -1;
+			for (int x = 0; x < 8; x++)
+			{
+				for (int y = 0; y < 8; y++)
+				{
+					if (mas[x,y] == 0 && Possible(x, y))
+					{
+						tempo++;
+						if (a == tempo)
+						{
+							this.x = x;
+							this.y = y;
+							return true;
+						}
+					}
+				}
+			}
+			return false;
+		}
+
+		/// <summary>
+		/// проверяет может ли конь передвинуться на клетку с координатами Х и У за один ход
+		/// </summary>
+		/// <param name="x"></param>
+		/// <param name="y"></param>
+		/// <returns></returns>
+		private bool Possible(int x, int y)
+		{
+			if ((Math.Pow(this.x - x,2) + Math.Pow(this.y - y,2)) == 5)
+			{
+				return true;
+			}
+			return false;
+		}
+	}
+}
