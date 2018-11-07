e09fe1ebf4610740690272cba37f4d51d00368dc
LikynAI <Likyn2001@gmail.com>
2018-11-07 23:40:07 +0300

    Начал работу над дз

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Stack stack = new Stack();
+
+			for (int i = 0; i < 20; i++)
+			{
+				stack.Push(i);
+			}
+
+			for (int i = 0; i < 9; i++)
+			{
+				stack.Pop();
+			}
+
+			for (int i = 0; i < 9; i++)
+			{
+				stack.Push(i);
+			}
+
+			for (int i = 0; i < 13; i++)
+			{
+				Console.WriteLine(stack.Pop());
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
---
+++ Stack.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+	class Stack
+	{
+		int[] vs = new int[13];
+		int start = -1;
+		int end = 12;
+
+		public void Push(int a)
+		{
+			if (start == 12) { start = 0; }
+			else { start++; }
+			vs[start] = a;
+
+			if (start == end) { end++; }
+			if (end == 13) { end = 0; }
+		}
+
+		public int Peek()
+		{
+			if (start == end) { throw new IndexOutOfRangeException(); };
+			return vs[start];
+		}
+
+		public int Pop()
+		{
+			if (start == end) { throw new IndexOutOfRangeException(); };
+
+			int a = start;
+
+			if (start == 0) { start = 12; }
+			else { start--; }
+
+			if (start == end) { end--; }
+			if (end == -1) { end = 12; }
+
+			return vs[a];
+		}
+	}
+}
