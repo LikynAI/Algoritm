bfa48d0b3499c3a0b84f86cc1cb6c2952235e8bd
LikynAI <Likyn2001@gmail.com>
2018-11-12 00:11:36 +0300

    Исправил

--- Program.cs
+++ Program.cs
@@ -6,9 +6,7 @@ namespace ConsoleApp6
 	{
 		static void Main(string[] args)
 		{
-			Tree t = new Tree("10(7(5(1,6),8),15(12(11,14),16))",20);
-
-			t.rLR(1);
+			Console.WriteLine(HashFunction.Do("asef"));
 			Console.ReadLine();
 		}
 	}
