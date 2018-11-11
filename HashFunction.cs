2ce0a74653e77ef50f4c138426566a344dece7df
LikynAI <Likyn2001@gmail.com>
2018-11-11 22:21:32 +0300

    Поиск в Дереве и хэш функция

---
+++ HashFunction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+	public class HashFunction
+	{
+		public static int Do(string s)
+		{
+			int[] tempo = new int[s.Length];
+			for (int i = 0; i < s.Length; i++)
+			{
+				tempo[i] = s[i]*s[i]*(i+1)/3+17;
+			}
+
+			int Hash = 0;
+
+			foreach (int i in tempo)
+			{
+				Hash += i;
+			}
+
+			return Hash;
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Tree t = new Tree("10(7(5(1,6),8),15(12(11,14),16))",20);
+
+			Console.WriteLine(t.tree[t.Find(12)]);
+			Console.ReadLine();
+		}
+	}
+}
